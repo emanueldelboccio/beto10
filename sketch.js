9d0a8ba62562be8978f53dfc0a3f18a11e6418d6
emanueldelboccio <112333763+emanueldelboccio@users.noreply.github.com>
2023-04-28 10:12:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var backgroundimg,caio,beto
+var caioimg,betoimg
+function preload(){
+caioimg = loadImage = ("caio.jpg")
+betoimg = loadImage = ("beto.png")
+backgroundimg = loadImage = ("cel.jpg")
+}
+function setup(){
+createCanvas(400,400)
+}
+function draw(){
+background(backgroundimg)
+
+}
